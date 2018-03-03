c0546c1d0065d0687291f12b5ffd69629488ae31
Chris Gatewood <c.gatewood55@gmail.com>
2018-03-03 14:06:48 -0600

    challenge1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log('hello');
+
+var players = [
+    { name: "Bob", scores: [45,30 ] },
+     { name: "Bill", scores: [60,25 ] },
+      { name: "Charlie", scores: [70,35 ] }];
+
+      for (var i=0; i<players.length; i++){
+          var playerstring = "";
+          var playerString = players[i].name;
+          var playerString = players[i].scores;
+          console.log(playerString);
+        }
